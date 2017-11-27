498eecec5248fb41ec68974dea9861383a79e85c
bbtown <bll168@gwmail.gwu.edu>
2017-11-27 05:21:10 +0000

    updated IP for rethink

--- ProblemFeed.php
+++ ProblemFeed.php
@@ -91,7 +91,7 @@ function retrieve_id() {
 
                          <?php
                              require_once __DIR__.'/vendor/autoload.php';
-                             $conn = r\connect('52.20.101.105');
+                             $conn = r\connect('54.144.38.70');
                              $result = r\db("web")->table('problems')->run($conn);
                              foreach ($result as $doc) {
                                if(isset($doc['ProblemDescription'])) {
